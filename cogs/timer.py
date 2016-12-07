f03bd9c687ea446b7fca923f57978ef5870549b1
Ikusaba-san <eeveeday@gmail.com>
2016-12-06 22:00:44 -0800

    *, msg : str

--- cogs/timer.py
+++ cogs/timer.py
@@ -6,14 +6,14 @@ class Timer:
         self.bot = bot
         
     @commands.command(pass_context=True)
-    async def countdown(self, ctx, delay : int, *msg : str):
+    async def countdown(self, ctx, delay : int, *, msg : str):
         bot = self.bot
         await bot.delete_message(ctx.message)
         message = await bot.say("Counting down...")
         async for i in ACountdown(delay):
             await bot.edit_message(message, i)
         await bot.delete_message(message)
-        await bot.say(' '.join(msg))
+        await bot.say(msg)
 
 def setup(bot):
     bot.add_cog(Timer(bot))
