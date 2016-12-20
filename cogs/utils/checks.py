6eaeea7000944306dee08f627a32f25bf8b7fe49
Ikusaba-san <eeveeday@gmail.com>
2016-12-20 05:40:12 -0800

    fixed a bunch of typos and names

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -6,12 +6,13 @@ import discord.utils
 def is_owner_check(ctx):
     return ctx.message.author.id == '239110748180054017'
 
+_make_check = commands.check
 def is_owner():
-    return commands.check(is_owner_check)
+    return _make_check(is_owner_check)
 
-def check_permissions(ctx, perms):
+def permissions_check(ctx, **perms):
     msg = ctx.message
-    if is_owner_check(msg):
+    if is_owner_check(ctx):
         return True
 
     ch = msg.channel
@@ -20,22 +21,28 @@ def check_permissions(ctx, perms):
     return all(getattr(resolved, name, None) == value
                for name, value in perms.items())
 
-def role_or_permissions(ctx, check, **perms):
-    if check_permissions(ctx, perms):
-        return True
-
+def roles_check(ctx, *roles):
     ch = ctx.message.channel
     author = ctx.message.author
     if ch.is_private:
         return False
 
-    role = discord.utils.find(check, author.roles)
+    role = discord.utils.find((lambda r: r.name in roles), author.roles)
     return role is not None
 
-def roles_check(*roles, **perms):
+def roles_or_perms_check(ctx, *roles, **perms):
+    return roles_check(ctx, *roles) or permissions_check(ctx, **perms)
+
+def has_role(*roles):
+    return _make_check(lambda ctx: roles_check(ctx, *roles))
+
+def has_perms(**perms):
+    return _make_check(lambda ctx: permissions_check(ctx, **perms))
+
+def has_role_or_perms(*roles, **perms):
     def predicate(ctx):
-        return has_role_or_perms(ctx, lambda r: r.name in roles, **perms)
+        return roles_or_perms_check(ctx, *roles, **perms)
     return commands.check(predicate)
 
 def admin_or_permissions(**perms):
-    return roles_check('Bot Admin', **perms)
+    return has_role_or_perms('Bot Admin', **perms)
