59b328fe9a4470b1bdad7c2185f2df69668fb79e
Ikusaba-san <eeveeday@gmail.com>
2017-01-02 23:52:48 -0800

    finally made the list command

--- cogs/customcommands.py
+++ cogs/customcommands.py
@@ -3,6 +3,8 @@
 # So a dict is required
 #from collections import namedtuple
 from discord.ext import commands
+from operator import itemgetter
+
 from .utils import checks
 from .utils.database import Database
 
@@ -12,6 +14,22 @@ class CustomReactions:
         self.bot = bot
         self.db = Database.from_json(CC_FILE_NAME, factory_not_top_tier=dict)
 
+    def _pages(self, server):
+        pages = []
+        page = []
+        result_len = 0
+        page_append, pages_append = page.append, pages.append
+        for trig, react in sorted(self.db[server].items(), key=itemgetter(0)):
+            # If there is a cleaner way of doing this, please tell me.
+            result = trig + " => " + react
+            page_append(result)
+            result_len += len(result)
+            if result_len >= 1500:
+                pages_append(page)
+                page.clear()
+        pages_append(page)
+        return pages
+    
     @commands.group(aliases=["customcomm", "cc", "cr", "custreact"])
     async def customcommand(self):
         """Namespace for the custom commands"""
@@ -20,6 +38,8 @@ class CustomReactions:
     @customcommand.command(pass_context=True)
     async def add(self, ctx, trigger, *, reaction : str):
         """Adds a new custom reaction/trigger (depending on what bot you use)
+
+        The trigger must be put in quotes if you want spaces in your trigger.
         """
         server = ctx.message.server
         print(server)
@@ -33,10 +53,15 @@ class CustomReactions:
                 
     @customcommand.command(pass_context=True)
     async def list(self, ctx, page=0):
-        pass
+        server = ctx.message.server or "global"
+        await self.bot.say('\n'.join(self._pages(server)[page]))
     
     @customcommand.command(pass_context=True, aliases=['delete', 'del', 'rem',])
     async def remove(self, ctx, *, ccid : str):
+        """Removes a new custom reaction/trigger (depending on what bot you use)
+
+        The trigger must be put in quotes if you want spaces in your trigger.
+        """
         storage = self.db[ctx.message.server]
         if not storage:
             await self.bot.say("There are no commands for this server")
@@ -79,14 +104,10 @@ class CustomReactions:
             return await self.bot.say("{} was never a custom command".format(ccid))
             
     async def on_message(self, msg):
-        storage = self.db[msg.server]
-        if not storage:
-            return
+        storage = self.db[msg.server] or self.db["global"]
         reaction = storage.get(msg.content.lower())
         if reaction is not None:
-            print("passed")
             await self.bot.send_message(msg.channel, reaction)
         
 def setup(bot):
-    cc = CustomReactions(bot)
-    bot.add_cog(cc)
+    bot.add_cog(CustomReactions(bot))
