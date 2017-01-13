73f3e5d54e0106ce41fc60714b30004944f25c54
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-01-13 02:13:35 -0800

    removed redundant overrides

--- chiakibot.py
+++ chiakibot.py
@@ -15,61 +15,21 @@ def cog_prefix(cmd):
     cog = cmd.instance
     return (DEFAULT_CMD_PREFIX if cog is None
             else getattr(type(cog), '__prefix__', DEFAULT_CMD_PREFIX))
-            
+
 def str_prefix(cmd):
     prefix = cog_prefix(cmd)
     return prefix if isinstance(prefix, str) else '|'.join(prefix)
-            
+
 class ChiakiFormatter(commands.HelpFormatter):
     def _add_subcommands_to_page(self, max_width, commands):
-        commands = ((str_prefix(cmd) + name, cmd) for name, cmd in commands 
+        commands = ((str_prefix(cmd) + name, cmd) for name, cmd in commands
                     if name not in cmd.aliases)
         super()._add_subcommands_to_page(max_width, commands)
-        
+
     @property
-    def true_prefix(self):
-        return str_prefix(self.command)
-                
-    # There is only one line that is changed here...
-    # But that line actually affects how the thing is shown in help
-    def get_command_signature(self):
-        """Retrieves the signature portion of the help page."""
-        result = []
-        # This is the different line
-        # The original was: prefix = self.clean_prefix
-        # But since I'm allowing for module
-        prefix = self.true_prefix   
-        cmd = self.command
-        parent = cmd.full_parent_name
-        # Another line that was changed, just solely for readibility
-        if cmd.aliases:
-            aliases = '|'.join(cmd.aliases)
-            fmt = '{0}[{1.name}|{2}]'
-            if parent:
-                fmt = '{0}{3} [{1.name}|{2}]'
-            result.append(fmt.format(prefix, cmd, aliases, parent))
-        else:
-            name = prefix + cmd.name if not parent else prefix + parent + ' ' + cmd.name
-            result.append(name)
+    def clean_prefix(self):
+        return super().clean_prefix if self.is_bot() else str_prefix(self.command)
 
-        params = cmd.clean_params
-        if len(params) > 0:
-            for name, param in params.items():
-                if param.default is not param.empty:
-                    # We don't want None or '' to trigger the [name=value] case and instead it should
-                    # do [name] since [name=None] or [name=] are not exactly useful for the user.
-                    should_print = param.default if isinstance(param.default, str) else param.default is not None
-                    if should_print:
-                        result.append(f'[{name}={param.default}]')
-                    else:
-                        result.append(f'[{name}]')
-                elif param.kind == param.VAR_POSITIONAL:
-                    result.append(f'[{name}...]')
-                else:
-                    result.append(f'<{name}>')
-
-        return ' '.join(result)      
-        
     def format(self):
         """Handles the actual behaviour involved with formatting.
         To change the behaviour, this method should be overridden.
@@ -109,7 +69,7 @@ class ChiakiFormatter(commands.HelpFormatter):
             # we insert the zero width space there to give it approximate
             # last place sorting position.
             return f'{cmd.cog_name} (Prefix: {prefix})' if cmd.cog_name is not None else '\u200bNo Category:'
-        
+
         if self.is_bot():
             data = sorted(self.filter_command_list(), key=category)
             for category, commands in itertools.groupby(data, key=category):
@@ -132,26 +92,26 @@ class ChiakiFormatter(commands.HelpFormatter):
 
 def _get_databases(cog):
     return (attr for attr in vars(cog).values() if hasattr(attr, "dump"))
-    
+
 class ChiakiBot(commands.Bot):
     def __init__(self, command_prefix, formatter=None, description=None, pm_help=False, **options):
         super().__init__(command_prefix, formatter, description, pm_help, **options)
         self.loop.create_task(self.change_game())
         self.loop.create_task(self.dump_db_cycle())
-           
+
     # literally the only reason why I created a subclass
     def dump_databases(self):
         for cog in self.cogs.values():
             for db in _get_databases(cog):
                 db.dump()
-                
+
     async def logout(self):
         self.dump_databases()
         await super().logout()
 
     def get_member(self, id):
         return discord.utils.get(self.get_all_members(), id=id)
-        
+
     # Just some looping functions
     async def change_game(self):
         await self.wait_until_ready()
@@ -171,18 +131,18 @@ class ChiakiBot(commands.Bot):
             name = next(GAME_CHOICES)
             await self.change_presence(game=discord.Game(name=name))
             await asyncio.sleep(random.uniform(0.5, 10) * 60)
-    
+
     async def dump_db_cycle(self):
         await self.wait_until_ready()
         while not self.is_closed:
             self.dump_databases()
             print("all databases successfully dumped")
             await asyncio.sleep(600)
-    
+
 def _find_prefix_by_cog(bot, message):
     if not message.content:
         return DEFAULT_CMD_PREFIX
-    
+
     first_word = message.content.split()[0]
     try:
         maybe_cmd = re.search(r'(\w+)', first_word).group(1)
@@ -192,8 +152,7 @@ def _find_prefix_by_cog(bot, message):
     cmd = bot.get_command(maybe_cmd)
     if cmd is None:
         return DEFAULT_CMD_PREFIX
-    
+
     return cog_prefix(cmd)
 
-    
- 
+
