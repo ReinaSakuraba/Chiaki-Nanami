7bc47fe2f1cf51b19bd17085e74f8ba64986f1af
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-01-17 01:24:47 -0800

    Fixed a few errors

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,10 +1,8 @@
-import discord
+import asyncio
 import json
 import logging
-import os, os.path
-import re
-import traceback
-
+import os
+import uuid
 
 from .transformdict import IDAbleDict
 
@@ -12,14 +10,13 @@ DATA_PATH = 'data/'
 DB_PATH = DATA_PATH + 'databases/'
 TEMP_FILE_NUM_PADDING = 8
 
-def _load_json(name):
+def _load_json(name, object_hook=None):
     try:
         with open(name, encoding='utf-8') as f:
-            return json.load(f)
+            return json.load(f, object_hook=object_hook)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
         return {}
 
-def _dump_json(name, data):
 class Database(IDAbleDict):
     # json sucks.
     # My idea was to put the actual discord objects (such as the actual server)
@@ -33,21 +30,23 @@ class Database(IDAbleDict):
         super().__init__(default_factory, mapping)
         self.logger = logging.getLogger("nanami_data")
         # Pay no attention to this copyness
+        self.loop = kwargs.pop('loop', asyncio.get_event_loop())
         self.object_hook = kwargs.pop('object_hook', None)
         self.encoder = kwargs.pop('encoder', None)
         self.lock = asyncio.Lock()
-        
+
     def __repr__(self):
-        return ("Database(name={0.name}, default_factory={0.default_factory},\n"
+        return ("Database(name='{0.name}', default_factory={0.default_factory.__name__}, "
                 "object_hook={0.object_hook}, encoder={0.encoder})").format(self)
-    
+
     def _dump(self, path=DB_PATH):
         name = path + self.name
+        check_dir(os.path.dirname(name))
         tmp_fname = f'{name}-{uuid.uuid4()}.tmp'
-        with open(name, encoding='utf-8', mode="w") as f:
-            json.dump(data, f, indent=4, sort_keys=True,
-                separators=(',', ' : '), object_hook=self.object_hook)
-    
+        with open(tmp_fname, encoding='utf-8', mode="w") as f:
+            json.dump(self, f, indent=4, sort_keys=True,
+                separators=(',', ' : '), cls=self.encoder)
+
         try:
             _load_json(tmp_fname)
         except json.decoder.JSONDecodeError:
@@ -56,25 +55,24 @@ class Database(IDAbleDict):
                                   "The original file is unaltered."
                                   "".format(filename))
             return False
-        os.replace(tmp_fname, name + ".json")
+        os.replace(tmp_fname, name + (".json" * (not name.endswith(".json"))))
         return True
-    
+
     async def dump(self, path=DB_PATH):
         with await self.lock:
             await self.loop.run_in_executor(None, self._dump, path)
-
-    @discord.utils.deprecated("db.get(id)")
-    def get_storage(self, id_ : str):
-        return self.get(id_)
-        
+            
     @classmethod
-    def from_json(cls, filename, path=DB_PATH, default_factory=None):
-        data = _load_json(path + filename)
-        return cls(filename, default_factory, data)
+    def from_json(cls, filename, path=DB_PATH, default_factory=None, **kwargs):
+        data = _load_json(path + filename, kwargs.get('object_hook'))
+        return cls(filename, default_factory, data, **kwargs)
 
+def check_dir(dir_):
+    os.makedirs(dir_, exist_ok=True)
+    
 def check_data_dir(dir_):
     os.makedirs(DATA_PATH + dir_, exist_ok=True)
-  
+
 def check_database_dir(dir_):
     os.makedirs(DB_PATH + dir_, exist_ok=True)
-    
+
