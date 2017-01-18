1e619dadfd6d02970394e1760f9f8a431520b388
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-01-17 16:50:40 -0800

    decided to remove patching
    Replaced Colour.from_rgb with t

--- cogs/diepio/utils.py
+++ cogs/diepio/utils.py
@@ -1,14 +1,12 @@
 from discord import Colour
 
-from ..utils import patching
-
 # hard-coding the colours because there is no color info of each mode from the webpage
 MODE_COLOURS = {
-    'FFA'     : Colour.from_rgb(113, 204, 200),
-    '2-TDM'   : Colour.from_rgb(180, 255, 142),
-    '4-TDM'   : Colour.from_rgb(255, 142, 142),
-    'Maze'    : Colour.from_rgb(181, 142, 255),
-    'Sandbox' : Colour.from_rgb(251, 142, 255),
+    'FFA'     : Colour(0x71CCC8), # Colour.from_rgb(113, 204, 200),
+    '2-TDM'   : Colour(0xB4FF8E), # Colour.from_rgb(180, 255, 142),
+    '4-TDM'   : Colour(0xFF8E8E), # Colour.from_rgb(255, 142, 142),
+    'Maze'    : Colour(0xB58EFF), # Colour.from_rgb(181, 142, 255),
+    'Sandbox' : Colour(0xFB8EFF), # Colour.from_rgb(251, 142, 255),
     }
 
 def mode_colour(mode):
--- cogs/utils/patching.py
+++
@@ -1,9 +0,0 @@
-# Monkey-patching things.
-
-import discord
-
-def from_rgb(cls, r, g, b):
-    return cls(r << 16 | g << 8 | b)
-
-discord.Colour.from_rgb = classmethod(from_rgb)
-del from_rgb
