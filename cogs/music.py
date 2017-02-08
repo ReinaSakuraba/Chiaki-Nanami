9975cfd007c13ce48636505ecb809719e78a4480
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-02-08 10:17:15 -0800

    renamed musictest to music; more features
    - bot now automatically disconnect after a certain time if no one requested a song
    - link sanitization to avoid people sending links that aren't youtube or soundcloud
     (because a ton of links are supported by youtube_dl)
    - duration now shows fully, not just in minutes and hours
    - force-skipping
    - cd removed in favour of just modifying the path environment due to races
      where someone requests a song while another is being processed

--- cogs/music.py
+++ cogs/music.py
@@ -1,13 +1,22 @@
 import asyncio
 import collections
 import discord
+import enum
 import random
+import os
+import re
 import time
 
+from collections import defaultdict
 from discord.ext import commands
 
-from .utils.cd import cd
-from .utils.misc import str_join
+from .utils import checks
+from .utils.errors import ResultsNotFound
+from .utils.misc import str_join, duration_units
+
+# Can't use a with statement here...
+_temporary_env = dict(os.environ)
+os.environ["PATH"] += os.pathsep + r"dependencies\ffmpeg\bin"
 
 # Yes I know this is a copy of https://github.com/Rapptz/discord.py/blob/master/examples/playlist.py
 # But hey, no point reinventing the wheel
@@ -25,55 +34,90 @@ def _time(self):
 
 discord.voice_client.StreamPlayer.time = property(_time)
 
-class ShuffableQueue(asyncio.Queue):
-    # Probably not safe
+def _get_info(player):
+    ytdl = player.yt
+    info = ytdl.extract_info(player.url, download=False)
+    if "entries" in info:
+        info = info['entries'][0]
+    return info
+
+class AcceptableURL(enum.Enum):
+    YOUTUBE = re.compile(r'^(https?\:\/\/)?(www\.|m\.)?(youtube\.com|youtu\.?be)\/.+$')
+    SOUNDCLOUD = sc_url = re.compile(r'^(https?\:\/\/)?(www\.)?(soundcloud\.com\/)')
+
+    @classmethod
+    def sanitize(cls, url):
+        for link_pattern in cls:
+            if link_pattern.value.match(url):
+                break
+        else:
+            raise commands.BadArgument("The given url is not acceptable for playing music")
+
+class MusicQueue(asyncio.Queue):
+    # Probably not thread-safe
     def __delitem__(self, idx):
         del self._queue[idx]
-        
+
     def __getitem__(self, idx):
         return self._queue[idx]
 
     def __setitem__(self, idx, val):
         self._queue[idx] = val
-    
+
     def __iter__(self):
         return iter(self._queue)
 
     def clear(self):
         self._queue.clear()
-    
+
     def shuffle(self):
-        queue = list(self._queue)
-        random.shuffle(queue)
-        self._queue = collections.deque(queue)
-        
+        random.shuffle(self._queue)
+
 class VoiceEntry:
     def __init__(self, message, player):
         self.requester = message.author
         self.channel = message.channel
         self.player = player
+        self.info = _get_info(player)
+        AcceptableURL.sanitize(self.info['webpage_url'])
 
     def __str__(self):
         fmt = '**{0.title}** uploaded by {0.uploader} and requested by {1.display_name}'
         duration = self.player.duration
         if duration:
-            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
+            fmt = fmt + f' [length: {duration_units(duration)}]'
         return fmt.format(self.player, self.requester)
+
+    @property
+    def embed(self):
+        requester = self.requester
+        avatar = requester.avatar_url or requester.default_avatar_url
+        player = self.player
+        info = _get_info(player)
+        print(info['thumbnails'][0]['url'] == info['thumbnail'])
+
+        return (discord.Embed(title=player.title, description=f"Uploaded by {player.uploader}", url=info['webpage_url'])
+               .set_author(name=f"Requested by {requester.display_name}", icon_url=avatar)
+               .set_thumbnail(url=info.get('thumbnail'))
+               .set_footer(text=f'Duration: {duration_units(player.duration)}')
+               )
 
     @property
     def required_skip_votes(self):
         return len(self.channel.voice_members)
 
+WAIT_FOR_SONG_TIME = 60
 class VoiceState:
     def __init__(self, bot):
         self.current = None
         self.voice = None
         self.bot = bot
         self.play_next_song = asyncio.Event()
-        self.songs = ShuffableQueue()
+        self.got_next_song = asyncio.Event()
+        self.songs = MusicQueue()
         self.skip_votes = set() # a set of user_ids that voted
         self.audio_player = self.bot.loop.create_task(self.audio_player_task())
-        
+
     def is_playing(self):
         if self.voice is None or self.current is None:
             return False
@@ -87,8 +131,8 @@ class VoiceState:
             return
         self.skip_votes.clear()
         if self.is_playing():
-            self.player.stop()
-        
+            self.current.player.stop()
+
     def shuffle(self):
         self.songs.shuffle()
 
@@ -98,16 +142,37 @@ class VoiceState:
     def toggle_next(self):
         self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
 
+    async def get_next_song(self):
+        try:
+            self.current = await asyncio.wait_for(self.songs.get(), WAIT_FOR_SONG_TIME)
+        except asyncio.TimeoutError:
+            await self.stop()
+        else:
+            self.bot.loop.call_soon_threadsafe(self.got_next_song.set)
+
+    async def stop(self):
+        await self.bot.send_message(self.current.channel, "Disconnecting...")
+        await self.voice.disconnect()
+        self.voice = None
+        self.current = None
+
+    async def wait_until_next_song(self):
+        await self.get_next_song()
+        await self.got_next_song.wait()
+        if self.current is None:
+            await self.get_next_song()
+
     async def audio_player_task(self):
         while True:
+            self.got_next_song.clear()
             self.play_next_song.clear()
             if self.current is not None:
                 await self.bot.send_message(self.current.channel, 'Finished playing ' + str(self.current))
-            self.current = await self.songs.get()
-            await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
+            await self.wait_until_next_song()
+            await self.bot.send_message(self.current.channel, embed=self.current.embed)
             self.current.player.start()
             await self.play_next_song.wait()
-            
+
     @property
     def player(self):
         return self.current.player
@@ -120,7 +185,7 @@ class Music:
     __prefix__ = '&'
     def __init__(self, bot):
         self.bot = bot
-        self.voice_states = collections.defaultdict(lambda: VoiceState(self.bot))
+        self.voice_states = defaultdict(lambda: VoiceState(self.bot))
 
     def get_voice_state(self, server):
         return self.voice_states[server]
@@ -185,13 +250,11 @@ class Music:
         }
 
         if state.voice is None:
-            success = await ctx.invoke(self.summon)
-            if not success:
+            if not await ctx.invoke(self.summon):
                 return
 
         try:
-            with cd(r"dependencies\ffmpeg\bin"):
-                player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
+            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
         except Exception as e:
             fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
             await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
@@ -200,6 +263,7 @@ class Music:
             entry = VoiceEntry(ctx.message, player)
             await self.bot.say('Enqueued ' + str(entry))
             await state.songs.put(entry)
+            state.got_next_song.set()
 
     @commands.command(pass_context=True, no_pm=True)
     async def volume(self, ctx, value : int):
@@ -228,6 +292,7 @@ class Music:
             player.resume()
 
     @commands.command(pass_context=True, no_pm=True)
+    @checks.is_mod()
     async def stop(self, ctx):
         """Stops playing audio and leaves the voice channel.
 
@@ -248,7 +313,7 @@ class Music:
             pass
 
     @commands.command(pass_context=True, no_pm=True)
-    async def skip(self, ctx, num: int=None, force_skip=False):
+    async def skip(self, ctx, num: int=None, force_skip=''):
         """Vote to skip a song. The song requester can automatically skip.
 
         3 skip votes are needed for the song to be skipped.
@@ -258,7 +323,10 @@ class Music:
             num = None
             force_skip = True
         else:
+            if num is not None:
+                num -= num > 0
             force_skip = force_skip == '-f'
+
 
         state = self.get_voice_state(ctx.message.server)
         if not state.is_playing():
@@ -268,24 +336,28 @@ class Music:
         try:
             current = state.current if num is None else state.songs[num]
         except IndexError:
-            await self.bot.say("Song {} doesn't exist".format(num))
-            return
-        
-        voter = ctx.message.author
-        if voter == current.requester:
-            await self.bot.say('Requester requested skipping song...')
+            raise ResultsNotFound(f"Song #{num} doesn't exist")
+
+        async def skip(msg):
+            await self.bot.say(msg)
             state.skip(num)
             await self.bot.say("Removing " + str(current))
+
+        if force_skip and checks.role_predicate(ctx, str(checks.ChiakiRole.mod)):
+           await skip('Force skip...')
+
+        voter = ctx.message.author
+        if voter == current.requester:
+            await skip('Requester requested skipping song...')
         elif voter.id not in state.skip_votes:
             state.skip_votes.add(voter.id)
             total_votes = len(state.skip_votes)
             required_skip_votes = current.required_skip_votes
             if total_votes >= required_skip_votes:
-                await self.bot.say('Skip vote passed, skipping song...')
-                state.skip(num)
-                await self.bot.say("Removing " + str(current))
+                await skip('Skip vote passed, skipping song...')
             else:
-                await self.bot.say('Skip vote added, currently at [{}/{}]'.format(total_votes, required_skip_votes))
+                await self.bot.say(('Skip vote added, currently at [**{}**/{}]'
+                                   ).format(total_votes, required_skip_votes))
         else:
             await self.bot.say('You have already voted to skip this song.')
 
@@ -298,7 +370,7 @@ class Music:
             await self.bot.say('Not playing anything.')
         else:
             skip_count = len(state.skip_votes)
-            fmt = 'Now {0.player.time}s into playing {0} [skips: {1}/{0.required_skip_votes}]'
+            fmt = 'Now {duration_units(0.player.time)} into playing {0} [skips: {1}/{0.required_skip_votes}]'
             await self.bot.say(fmt.format(state.current, skip_count))
 
     @commands.group(pass_context=True, no_pm=True)
@@ -307,6 +379,7 @@ class Music:
         pass
 
     @queue.command(pass_context=True, no_pm=True)
+    @checks.is_mod()
     async def clear(self, ctx):
         """Clears the queue
 
@@ -315,7 +388,7 @@ class Music:
         """
         state = self.get_voice_state(ctx.message.server)
         state.clear()
-    
+
     @queue.command(pass_context=True, no_pm=True)
     async def shuffle(self, ctx):
         """Shuffle the queue"""
@@ -334,5 +407,20 @@ class Music:
         state = self.get_voice_state(ctx.message.server)
         await self.bot.say(str_join('\n', state.songs))
 
+    @queue.command(pass_context=True, no_pm=True)
+    async def nextsong(self, ctx):
+        """Shows the next song that will be played"""
+
+        state = self.get_voice_state(ctx.message.server)
+        msg = str(state.songs[-1]) if state.songs else "There is no song pending... I think."
+        await self.bot.say(msg)
+
+
 def setup(bot):
     bot.add_cog(Music(bot))
+
+# Can someone tell Danny that __unload isn't being called...?
+def teardown(bot):
+    bot.get_cog("Music")._Music__unload()
+    os.environ.clear()
+    os.environ.update(_temporary_env)
