85812cc958667464482421ea14da6b5c9f5bcf7b
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-02-08 01:41:58 -0800

    colour from image
    ...asynchronously

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,16 +1,16 @@
+import aiohttp
 import discord
-import itertools
 import json
-import os
-import random
 
 from collections import defaultdict, deque
 from discord.ext import commands
 from operator import attrgetter, itemgetter
 
 from .utils import converter
-from .utils.misc import str_join, filter_attr, status_color, image_from_url, nice_time, ordinal
-from .utils.paginator import DelimPaginator, iterable_say
+from .utils.compat import url_color, user_color
+from .utils.errors import ResultsNotFound
+from .utils.misc import str_join, nice_time, ordinal
+from .utils.paginator import iterable_say
 
 def _ilen(gen):
     return sum(1 for _ in gen)
@@ -20,36 +20,34 @@ def _icon_embed(idable, url, name):
             .set_footer(text=f"ID: {idable.id}"))
     return embed.set_image(url=url) if url else embed
 
-def _user_embed(member):
-    avatar_url = member.avatar_url or member.default_avatar_url
-    playing = f"Playing **{member.game}**"
-    real_name = f"{member.name}#{member.discriminator}"
-    roles = sorted(member.roles[1:], key=attrgetter("position"), reverse=True)
-    server = member.server
-    embed = discord.Embed(colour=member.colour, description=playing)
-    for name, value in [("Real Name", real_name),
-                        (f"Joined {server} at", nice_time(member.joined_at)),
-                        ("Created at", nice_time(member.created_at)),
-                        ("Highest role", member.top_role), ("Roles", str_join(', ', roles) or "-no roles-"),
-                        ]:
-        embed.add_field(name=name, value=value)
-    embed.set_author(name=member.display_name, icon_url=avatar_url)
-    embed.set_footer(text=f"ID: {member.id}")
-    embed.set_thumbnail(url=avatar_url)
-
-    return embed
-
 async def _mee6_stats(session, member: discord.member):
     server = member.server
     async with session.get(f"https://mee6.xyz/levels/{server.id}?json=1&limit=-1") as r:
         levels = await r.json()
     players = levels["players"]
     user_stats = discord.utils.find(lambda e: e.get("id") == member.id, players)
-    # Because lists start at 0
     if not user_stats:
-        return None
+        raise ResultsNotFound(f"This user ({member}) does not have a mee6 level. :frowning:")
+    # Because lists start at 0
     user_stats["rank"] = players.index(user_stats) + 1
     return user_stats
+
+async def _user_embed(member):
+    avatar_url = member.avatar_url or member.default_avatar_url
+    playing = f"Playing **{member.game}**"
+    roles = sorted(member.roles[1:], reverse=True)
+    server = member.server
+    colour = await user_color(member)
+    return  (discord.Embed(colour=colour, description=playing)
+            .set_thumbnail(url=avatar_url)
+            .set_author(name=member.display_name, icon_url=avatar_url)
+            .add_field(name="Real Name", value=str(member))
+            .add_field(name=f"Joined {server} at", value=nice_time(member.joined_at))
+            .add_field(name="Created at", value=nice_time(member.created_at))
+            .add_field(name="Highest role", value=member.top_role)
+            .add_field(name="Roles", value=str_join(', ', roles) or "-no roles-", inline=False)
+            .set_footer(text=f"ID: {member.id}")
+            )
 
 class Meta:
     """Info related commands"""
@@ -58,13 +56,19 @@ class Meta:
     def __init__(self, bot):
         self.bot = bot
         self.cmd_history = defaultdict(lambda: deque(maxlen=50))
+        # Could use self.bot.http.session but that's incredibly bad practice
+        # (not sure why though...)
+        self.session = aiohttp.ClientSession()
+
+    def __unload(self):
+        # Pray it closes
+        self.bot.loop.create_task(self.session.close())
 
     @commands.command(pass_context=True, no_pm=True)
     async def uinfo(self, ctx, *, user : discord.Member=None):
-        """Gets some useful info because why not"""
+        """Gets some basic userful info because why not"""
         if user is None:
             user = ctx.message.author
-        print(type(user))
         fmt = ("    Name: {0.name}\n"
                "      ID: {0.id}\n"
                " Hashtag: {0.discriminator}\n"
@@ -74,8 +78,7 @@ class Meta:
                "   Roles: {1}\n"
                "  Status: {0.status}\n"
                )
-        roles = list(map(str, user.roles[1:]))[::-1]
-        roles = ', '.join(roles)
+        roles = str_join(', ', reversed(user.roles[1:]))
         await self.bot.say("```\n{}\n```".format(fmt.format(user, roles)))
 
     @commands.group(pass_context=True)
@@ -100,43 +103,43 @@ class Meta:
         message = await self.bot.say("Fetching data, please wait...")
         if member is None:
             member = ctx.message.author
-
         avatar_url = member.avatar_url or member.default_avatar_url
+
         try:
             stats = await _mee6_stats(self.bot.http.session, member)
         except json.JSONDecodeError:
-            await self.bot.say("No stats found. You don't have mee6 in this server... I think.")
-            return
-        if not stats:
-            await self.bot.say(f"This user ({member}) does not have a mee6 level. :frowning:")
-            return
+            raise ResultsNotFound("No stats found. You don't have mee6 in this server... I think.")
+        finally:
+            await self.bot.delete_message(message)
 
         description = f"Currently sitting at {stats['rank']}!"
         xp_progress = "{xp}/{lvl_xp} ({xp_percent}%)".format(**stats)
         xp_remaining = stats['lvl_xp'] - stats['xp']
+        colour = await user_color(member)
 
-        mee6_embed = discord.Embed(colour=member.colour, description=description)
+        mee6_embed = (discord.Embed(colour=colour, description=description)
+                     .set_author(name=member.display_name, icon_url=avatar_url)
+                     .set_thumbnail(url=avatar_url)
+                     .add_field(name="Level", value=stats['lvl'])
+                     .add_field(name="Total XP", value=stats['total_xp'])
+                     .add_field(name="Level XP",  value=xp_progress)
+                     .add_field(name="XP Remaining to next level",  value=xp_remaining)
+                     .set_footer(text=f"ID: {member.id}")
+                     )
 
-        mee6_embed.set_author(name=member.display_name, icon_url=avatar_url)
-        mee6_embed.set_thumbnail(url=avatar_url)
-        mee6_embed.add_field(name="Level", value=stats['lvl'])
-        mee6_embed.add_field(name="Total XP", value=stats['total_xp'])
-        mee6_embed.add_field(name="Level XP",  value=xp_progress)
-        mee6_embed.add_field(name="XP Remaining to next level",  value=xp_remaining)
-        mee6_embed.set_footer(text=f"ID: {member.id}")
-
-        await self.bot.delete_message(message)
         await self.bot.say(embed=mee6_embed)
 
     @info.command(pass_context=True)
     async def role(self, ctx, *, role: converter.ApproximateRole):
-        get_bool_as_ans = lambda b: "Yes" if b else "No"
+        server = ctx.message.server
+        get_bool_as_ans = lambda b: "YNeos"[not b::2]
+        prefix = self.bot.str_prefix(self, server)
 
-        has_roles = [mem for mem in ctx.message.server.members if role in mem.roles]
+        has_roles = [mem for mem in server.members if role in mem.roles]
         member_amount = len(has_roles)
         if member_amount > 20:
             members_name = "Members"
-            members_value = f"{member_amount} (use ?inrole '{role}' to figure out who's in that role)"
+            members_value = f"{member_amount} (use {prefix}inrole '{role}' to figure out who's in that role)"
         else:
             members_name = f"Members ({member_amount})"
             members_value = str_join(", ", has_roles)
@@ -146,19 +149,20 @@ class Meta:
         permission_binary = "{0:32b}".format(permissions)
         str_position = ordinal(role.position)
         nice_created_at = nice_time(role.created_at)
-        description = f"Just chilling as the {str_position} role"
+        description = f"Just chilling as {server}'s {str_position} role"
         footer = f"Created at: {nice_created_at} | ID: {role.id}"
 
         # I think there's a way to make a solid color thumbnail, idk though
-        role_embed = discord.Embed(title=role.name, colour=role.colour, description=description)
-        role_embed.add_field(name="Colour", value=hex_role_color)
-        role_embed.add_field(name="Permissions", value=permissions)
-        role_embed.add_field(name="Permissions (as binary)", value=permission_binary)
-        role_embed.add_field(name="Mentionable?", value=get_bool_as_ans(role.mentionable))
-        role_embed.add_field(name="Displayed separately?", value=get_bool_as_ans(role.hoist))
-        role_embed.add_field(name="Integration role?", value=get_bool_as_ans(role.managed))
-        role_embed.add_field(name=members_name, value=members_value, inline=False)
-        role_embed.set_footer(text=footer)
+        role_embed = (discord.Embed(title=role.name, colour=role.colour, description=description)
+                     .add_field(name="Colour", value=hex_role_color)
+                     .add_field(name="Permissions", value=permissions)
+                     .add_field(name="Permissions (as binary)", value=permission_binary)
+                     .add_field(name="Mentionable?", value=get_bool_as_ans(role.mentionable))
+                     .add_field(name="Displayed separately?", value=get_bool_as_ans(role.hoist))
+                     .add_field(name="Integration role?", value=get_bool_as_ans(role.managed))
+                     .add_field(name=members_name, value=members_value, inline=False)
+                     .set_footer(text=footer)
+                     )
 
         await self.bot.say(embed=role_embed)
 
@@ -169,36 +173,33 @@ class Meta:
         member_count = len(server.members)
         is_large = "(Very large!)" * server.large
         members_comment = f"{member_count} members {is_large}"
-        server_icon_url = server.icon_url
-        # TODO: Find the average color of the server's icon and use that for the embed color
-        colour = random.randrange(255 ** 3)
+        icon = server.icon_url
         highest_role = server.role_hierarchy[0]
         nice_created_at = nice_time(server.created_at)
         footer = f"Created at: {nice_created_at} | ID: {server.id}"
+        prefix = self.bot.str_prefix(self, server)
 
         if member_count < 20:
             member_field_name = f"Members ({member_count})"
             member_field_value = ', '.join([mem.mention for mem in server.members])
         else:
             member_field_name = f"Members"
-            member_field_value = f"{member_count} (use '?info server members' to figure out the members)"
-        server_embed = discord.Embed(title=server.name, colour=colour, description=members_comment)
-        if server_icon_url:
-            server_embed.set_thumbnail(url=server_icon_url)
-        server_embed.add_field(name="Owner", value=server.owner)
-        server_embed.add_field(name="Highest Role", value=highest_role)
-        server_embed.add_field(name="Channel Count", value=len(server.channels))
-        server_embed.add_field(name="Role Count", value=len(server.roles))
-        server_embed.add_field(name=member_field_name, value=member_field_value)
-        server_embed.set_footer(text=footer)
+            member_field_value = f"{member_count} (use '{prefix}info server members' to figure out the members)"
+
+        server_embed = (discord.Embed(title=server.name, description=members_comment)
+                       .add_field(name="Owner", value=server.owner)
+                       .add_field(name="Highest Role", value=highest_role)
+                       .add_field(name="Channel Count", value=len(server.channels))
+                       .add_field(name="Role Count", value=len(server.roles))
+                       .add_field(name=member_field_name, value=member_field_value)
+                       .set_footer(text=footer))
+        if icon:
+            server_embed.set_thumbnail(url=icon)
+            server_embed.colour = await url_color(icon)
         await self.bot.say(embed=server_embed)
 
-    @info.group(pass_context=True)
+    @info.group(pass_context=True, no_pm=True)
     async def server(self, ctx):
-        print(ctx.subcommand_passed, ctx.invoked_subcommand)
-        if not ctx.message.server:
-            await self.bot.say("You are not in a server. Why are you using this command?")
-            return
         if ctx.subcommand_passed == "server":
             await self._default_server_info(ctx)
         elif ctx.invoked_subcommand is None:
@@ -228,7 +229,7 @@ class Meta:
         """Gets some userful info because why not"""
         if member is None:
             member = ctx.message.author
-        await self.bot.say(embed=_user_embed(member))
+        await self.bot.say(embed=await _user_embed(member))
 
     @commands.command(name="you", pass_context=True)
     async def botinfo(self, ctx):
@@ -240,19 +241,19 @@ class Meta:
         app_icon_url = appinfo.icon_url
         print(app_icon_url)
         user_icon_url = user.avatar_url or user.default_avatar_url
-        bot_embed = (discord.Embed(title=appinfo.name, description=description, colour=0xFFDDDD)
+        bot_embed = (discord.Embed(title=appinfo.name, description=description, colour=self.bot.colour)
                     .set_author(name=f"{user.name} | {appinfo.id}", icon_url=user_icon_url)
                     .add_field(name="Owner", value=appinfo.owner)
                     .add_field(name="Uptime", value=bot.str_uptime)
                     .add_field(name="Cogs running", value=len(bot.cogs))
                     .add_field(name="Databases", value=len(bot.databases))
                     .add_field(name="Servers", value=len(bot.servers))
-                    .add_field(name="Members I see", value=_ilen(bot.get_all_members()))
+                    .add_field(name="Members I see", value=len(set(bot.get_all_members())))
                     .add_field(name="Channels I see", value=_ilen(bot.get_all_channels()))
                     )
         if app_icon_url:
             bot_embed.set_thumbnail(url=app_icon_url)
-        for name, value in sorted(bot.commands_counter.items(), key=itemgetter(0)):
+        for name, value in sorted(bot.counter.items(), key=itemgetter(0)):
             bot_embed.add_field(name=name, value=value)
         await self.bot.say(embed=bot_embed)
 
@@ -279,26 +280,10 @@ class Meta:
 
         The permission is case insensitive.
         """
-        print("executed")
         perm_attr = perm.replace(' ', '_').lower()
-        fmt = "Here are the roles who have the {} perm".format(perm.title())
         roles_that_have_perms = [role for role in ctx.message.server.roles
                                  if getattr(role.permissions, perm_attr)]
-        role_fmt = "```css\n{}```"
-        await self.bot.say(fmt + role_fmt.format(str_join(', ', roles_that_have_perms)))
-
-    @commands.command(pass_context=True)
-    async def permroles(self, ctx, *, perm: str):
-        """
-        Checks which roles have a particular permission
-
-        The permission is case insensitive.
-        """
-        print("executed")
-        perm_attr = perm.replace(' ', '_').lower()
         fmt = "Here are the roles who have the {} perm".format(perm.title())
-        roles_that_have_perms = [role for role in ctx.message.server.roles
-                                 if getattr(role.permissions, perm_attr)]
         role_fmt = "```css\n{}```"
         await self.bot.say(fmt + role_fmt.format(str_join(', ', roles_that_have_perms)))
 
@@ -306,26 +291,25 @@ class Meta:
     async def avatar(self, ctx, *, user : converter.ApproximateUser=None):
         if user is None:
             user = ctx.message.author
-
-        nick = ' ({})'.format(user.nick) * (user.nick is not None)
-        av_fmt = f"**{user.name}#{user.discriminator}{nick}'s avatar**"
         avatar_url = user.avatar_url or user.default_avatar_url
-        avatar = user.avatar or user.default_avatar
+        colour = await user_color(user)
+        nick = getattr(user, 'nick', None)
+        description = f"*(Also known as \"{nick}\")*" * bool(nick)
 
-        # Pay no attention to this ugliness
-        image, name = await image_from_url(avatar_url, avatar, self.bot.http.session)
-        await self.bot.send_file(ctx.message.channel, name, content=av_fmt)
-        os.remove(name)
-        image.close()
+        av_embed = (discord.Embed(colour=colour, description=description)
+                   .set_author(name=f"{user}'s Avatar", icon_url=avatar_url, url=avatar_url)
+                   #.add_field(name="Link", value=f"[Click me for avatar!]({avatar_url})")
+                   .set_image(url=avatar_url)
+                   .set_footer(text=f"ID: {user.id}")
+                   )
+        await self.bot.say(embed=av_embed)
 
     @commands.command(pass_context=True)
     async def cmdhistory(self, ctx):
         """Displays up to the last 50 commands you've input"""
         history = self.cmd_history[ctx.message.author]
-        if not history:
-            msg = "You have not input any commands..."
-        else:
-            msg = f"Your last {len(history)} commands:\n```\n{', '.join(history)}```"
+        msg = (f"Your last {len(history)} commands:\n```\n{', '.join(history)}```"
+               if history else "You have not input any commands...")
         await self.bot.say(msg)
 
     async def on_command(self, cmd, ctx):
@@ -333,3 +317,4 @@ class Meta:
 
 def setup(bot):
     bot.add_cog(Meta(bot))
+
---
+++ cogs/utils/compat.py
@@ -0,0 +1,98 @@
+"""Compatibility incase some libraries weren't imported"""
+import aiohttp
+import os
+
+from discord.ext import commands
+# someone make this standard plz
+try:
+    from aiocache import cached as async_cache
+except ImportError:
+    # http://stackoverflow.com/a/37627076
+    import asyncio
+    from collections import OrderedDict
+    from functools import _make_key, wraps
+
+    def async_cache(maxsize=128):
+        # support use as decorator without calling, for this case maxsize will
+        # not be an int
+        try:
+            real_max_size = int(maxsize)
+        except ValueError:
+            real_max_size = 128
+
+        boundless = maxsize is None
+        cache = OrderedDict()
+
+        async def run_and_cache(func, args, kwargs):
+            """Run func with the specified arguments and store the result
+            in cache."""
+            result = await func(*args, **kwargs)
+            cache[_make_key(args, kwargs, False)] = result
+            if not boundless and len(cache) > real_max_size:
+                cache.popitem(False)
+            return result
+
+        def wrapper(func):
+            @wraps(func)
+            def decorator(*args, **kwargs):
+                key = _make_key(args, kwargs, False)
+                if key in cache:
+                    # Some protection against duplicating calls already in
+                    # progress: when starting the call cache the future, and if
+                    # the same thing is requested again return that future.
+                    if isinstance(cache[key], asyncio.Future):
+                        return cache[key]
+                    else:
+                        f = asyncio.Future()
+                        f.set_result(cache[key])
+                        return f
+                else:
+                    task = asyncio.Task(run_and_cache(func, args, kwargs))
+                    cache[key] = task
+                    return task
+            return decorator
+
+        if callable(maxsize):
+            return wrapper(maxsize)
+        else:
+            return wrapper
+
+try:
+    from colorthief import ColorThief
+except ImportError:
+    ColorThief = None
+
+_chunk_size = 1024
+async def _write_from_url(url, filename):
+    async with aiohttp.get(url) as resp:
+        with open(filename, 'wb') as fd:
+            # TODO: Is there a way to make an async functools.partial?
+            while True:
+                chunk = await resp.content.read(_chunk_size)
+                if not chunk:
+                    break
+                fd.write(chunk)
+
+@async_cache(maxsize=_chunk_size * 8)
+async def _dominant_color_from_url(url, tmp_file='tmp.jpg'):
+    '''Downloads ths image file and analyzes the dominant color'''
+    await _write_from_url(url, tmp_file)
+    color_thief = ColorThief(tmp_file)
+    dominant_color = color_thief.get_color(quality=1)
+    os.remove(tmp_file)
+    return dominant_color
+
+# Let's hope Danny makes an extension for this
+def _color_from_rgb(r, g, b):
+    rgb = f"#{r:02x}{g:02x}{b:02x}"
+    return commands.ColourConverter(None, rgb).convert()
+
+async def url_color(url):
+    return _color_from_rgb(*(await _dominant_color_from_url(url)))
+
+async def user_color(user):
+    if ColorThief:
+        avatar = user.avatar_url or user.default_avatar_url
+        return _color_from_rgb(*(await _dominant_color_from_url(avatar)))
+    return user.colour
+user_colour = user_color
