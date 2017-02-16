aed34918572dd5a3a92d6ad926055f4d12a8ecee
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-02-16 11:22:37 -0800

    Fix decorator error
    Didn't know decorators can't aren't exactly able to detect if a function
    is a bound method.

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -4,18 +4,18 @@ from functools import wraps
 
 class PrivateMessagesOnly(commands.CommandError):
     """Exception raised when an operation only works in private message contexts."""
-    
+
 class InvalidUserArgument(commands.UserInputError):
     """Exception raised when the user inputs an invalid argument, even though conversion is successful."""
-    
+
 class ResultsNotFound(commands.UserInputError):
     """Exception raised when a search returns some form of "not found" """
-    
-def private_message_only(error_msg="This command can only be used in private messages"):
+
+def private_message_only(error_msg="This command can only be used in private messages", is_method=True):
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
-            if not args[inspect.ismethod(func)].message.channel.is_private:
+            if not args[is_method].message.channel.is_private:
                 raise PrivateMessagesOnly(error_msg)
             return await func(*args, **kwargs)
         return wrapper
