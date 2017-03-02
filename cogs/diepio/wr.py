948065d77eeef2782f65ed95c49dec5748457423
Ikusaba-san <eeveeday@gmail.com>
2017-03-01 21:27:30 -0800

    cleanup
    I removed the usage decorator since commands.Command now has a usage attribute I can play with
    Most of the changes are removing awkward line breaks, and replacing self.bot.say with ctx.send, since context is now passed in by default.
    I also used the mimetype of the url, which cleans up the worldrecord up a bunch.

--- cogs/diepio/wr.py
+++ cogs/diepio/wr.py
@@ -10,7 +10,6 @@ from operator import itemgetter
 
 from . import utils
 from ..utils.errors import InvalidUserArgument, ResultsNotFound
-from ..utils.misc import usage
 from ..utils.paginator import iterable_limit_say
 
 WR_RECORD_URL = 'https://dieprecords.moepl.eu/api/records/json'
@@ -67,6 +66,14 @@ def _replace_tank(tankname):
     t = tankname.title()
     return _alt_tank_names.get(t, t)
 
+async def url_is_image(url, session, *, img_filter=None):
+    if img_filter is None:
+        img_filter = {'png', 'jpeg', 'jpg', 'bmp', 'webp', 'gif', }
+    async with session.get(link) as response:
+        content = response.headers['Content-Type']
+        mime_type, _, subtype = content.partition("/")
+        return mime_type == "image" and subtype in img_filter
+        
 def _wr_embed(records):
     game_mode = records["gamemode"]
     url = utils.get_tank_icon(records["tankname"])
@@ -78,21 +85,13 @@ def _wr_embed(records):
             .add_field(name="Score", value=records["score"])
             .add_field(name="Full Score", value=records["scorefull"])
             .add_field(name="Date", value=str(approved_date))
+            .add_field(name="Proof!", value=records['submittedlink'])
             )
-
-    submitted_url = records["submittedlink"]
-    if "youtube" in submitted_url:
-        # No clean way to set the video yet
-        rest = submitted_url
-    elif not (submitted_url.endswith('.png') or submitted_url.endswith('.jpg')):
-        rest = submitted_url
-    else:
-        data.set_image(url=submitted_url)
-        rest = ''
-
-    return data, rest
+    return data
 
 class WRA:
+    """Commands related to the diep.io World Record Archive site"""
+    
     wr_records, tank_ids, gamemode_ids = {}, {}, {}
     _mode_translations = {
         'tdm': '2-TDM', '2tdm': '2-TDM', '2teams': '2-TDM',
@@ -168,9 +167,8 @@ class WRA:
 
         return get_records("desktop"), get_records("mobile")
 
-    @commands.command(aliases=['wr'])
-    @usage('wr desktop ffa sniper')
-    async def worldrecord(self, version: str, mode, *, tank : _replace_tank):
+    @commands.command(usage='desktop ffa sniper', aliases=['wr'])
+    async def worldrecord(self, ctx, version: str, mode, *, tank : _replace_tank):
         """Retrieves the world record from the WRA site
 
         version is version of diep.io (mobile or desktop)
@@ -182,13 +180,12 @@ class WRA:
         record = self._wr_mode(version, mode, tank)
 
         title = "**__{0} {gamemode} {tankname}__**".format(version.title(), **record)
-        embed, extra = _wr_embed(record)
-        await self.bot.say(title, embed=embed)
-        if extra:
-            await self.bot.say(extra)
+        embed = _wr_embed(record)
+        if await url_is_image(record['submittedlink'], self.session):
+            embed.set_image(url=records["submittedlink"])
+        await ctx.send(title, embed=embed)
 
-    @commands.command(pass_context=True)
-    @usage('wrtank sniper')
+    @commands.command(usage='sniper')
     async def wrtank(self, ctx, *, tank: _replace_tank):
         """Gives a summary of the WRs for a particular tank
 
@@ -211,9 +208,9 @@ class WRA:
         desktop_embed = embed_from_iterable("desktop", desktop)
         mobile_embed = embed_from_iterable("mobile", mobile) if mobile else None
 
-        await self.bot.say(title, embed=desktop_embed)
+        await ctx.send(title, embed=desktop_embed)
         if mobile_embed is not None:
-            await self.bot.say(embed=mobile_embed)
+            await ctx.send(embed=mobile_embed)
 
     async def _submit(self, name, tankid, gamemodeid, score, url):
         payload = {'inputname': name,
@@ -223,17 +220,15 @@ class WRA:
                    'proof': url}
         return await self.session.post('https://dieprecords.moepl.eu/api/submit/record', data=payload)
 
-    @commands.command()
-    @usage('submitwr "Junko Enoshima" destroyer desktop ffa 1666714 http://i.imgur.com/tIHCj5K.png')
-    async def submitwr(self, name: str, tank: _replace_tank, version : str, mode: _find_mode,
-                       score: int, url: str):
+    @commands.command(usage='"Junko Enoshima" destroyer desktop ffa 1666714 http://i.imgur.com/tIHCj5K.png')
+    async def submitwr(self, ctx, name, tank: _replace_tank, version, mode: _find_mode, score: int, url):
         """Submits a potential WR to the WR site
 
         The name and tank should be in quotes if you intend on putting spaces in either parameter
         (eg if you're gonna submit a WR under Junko Enoshima you should enter it as "Junko Enoshima")
         """
-        vers_ = version.lower()
-        record = await self._wr_mode(vers_, mode, tank)
+        vers = version.lower()
+        record = await self._wr_mode(vers, mode, tank)
 
         full_score = record["scorefull"]
         if score < 50000:
@@ -243,36 +238,33 @@ class WRA:
 
         submission = f'("{name}" "{tank}" {version} {score} <{url}>)'
 
-        async with await self._submit(name, tank_ids[tank_], gamemode_ids[vers_][mode_],
-                                      score, url) as response:
+        async with await self._submit(name, tank_ids[tank], gamemode_ids[vers][mode], score, url) as response:
             result = await response.json()
 
         msg = f"**{result['status'].title()}!** {result['content']}\n{submission}"
-        await self.bot.say(msg, delete_after=60)
+        await ctx.send(msg, delete_after=60)
 
     # TODO: Make this look pretty
     @commands.command()
-    async def gamemodes(self):
+    async def gamemodes(self, ctx):
         """All the gamemodes for diep.io"""
         def names(version):
             modes = sorted(self.gamemode_ids[version])
-            str_modes = ', '.join(modes)
-            return f"List of {version} gamemodes:\n{str_modes}\n"
-        await self.bot.say(names('desktop') + names('mobile'))
+            return f"List of {version} gamemodes:\n{', '.join(modes)}\n"
+        await ctx.send(names('desktop') + names('mobile'))
 
     @commands.command()
-    async def tanks(self):
+    async def tanks(self, ctx):
         """All the tanks for diep.io"""
-        await self.bot.say(', '.join(self.all_tanks()))
+        await ctx.send(', '.join(self.all_tanks()))
 
     @commands.command()
-    async def tankaliases(self):
+    async def tankaliases(self, ctx):
         """All the tanks for diep.io"""
         tank_aliases = '\n'.join([f"{k:<18} == {v}" for k, v in _alt_tank_names.items()])
-        await self.bot.say(f"```\n{tank_aliases}```")
+        await ctx.send(f"```\n{tank_aliases}```")
 
-    @commands.command(pass_context=True)
-    @usage('records Anokuu')
+    @commands.command(usage='Anokuu')
     async def records(self, ctx, *, name: str):
         """Finds all the diep.io WRs for a particular name"""
         records = await _load_json(self.session, f'https://dieprecords.moepl.eu/api/recordsByName/{name}')
@@ -311,7 +303,7 @@ class WRA:
         all_iterables = chain(current_header, desktop_current_str, mobile_current_str,
                               former_header, desktop_former_str, mobile_former_str)
 
-        await iterable_limit_say(all_iterables, bot=self.bot, ctx=ctx, prefix='', suffix='')
+        await iterable_limit_say(all_iterables, ctx=ctx, prefix='', suffix='')
 
 def setup(bot):
     bot.add_cog(WRA(bot))
