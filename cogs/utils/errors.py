2381ca6094396b9cf71a906da42b95b79482e345
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-03-02 23:56:31 -0800

    made private_messages_only a little clearer
    Completely forgot about commands.check

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -1,3 +1,4 @@
+import discord
 import inspect
 from discord.ext import commands
 from functools import wraps
@@ -11,12 +12,9 @@ class InvalidUserArgument(commands.UserInputError):
 class ResultsNotFound(commands.UserInputError):
     """Exception raised when a search returns some form of "not found" """
 
-def private_message_only(error_msg="This command can only be used in private messages", is_method=True):
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            if not args[is_method].message.channel.is_private:
-                raise PrivateMessagesOnly(error_msg)
-            return await func(*args, **kwargs)
-        return wrapper
-    return decorator
+def private_message_only(error_msg="This command can only be used in private messages"):
+    def predicate(ctx):
+        if isinstance(ctx.channel, discord.PrivateChannel):
+            return True
+        raise PrivateMessagesOnly(error_msg)
+    return commands.check(predicate)
