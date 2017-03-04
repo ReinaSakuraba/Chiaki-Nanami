0ec02bfbb8d107c68845d74476efbf48568e3b8c
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-03-03 23:58:29 -0800

    fixed AttributeError; added blanket-statements
    The rewrite never had a PrivateChannel type, or it did, but it was changed.
    Also added exceptions that Chiaki will say to the user, rather than just merely throwing

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -3,18 +3,21 @@ import inspect
 from discord.ext import commands
 from functools import wraps
 
-class PrivateMessagesOnly(commands.CommandError):
+class OutputtableException(commands.CommandError):
+    """Blanket exception for all exceptions with messages that the bot will say"""
+
+class PrivateMessagesOnly(OutputtableException):
     """Exception raised when an operation only works in private message contexts."""
 
-class InvalidUserArgument(commands.UserInputError):
+class InvalidUserArgument(OutputtableException):
     """Exception raised when the user inputs an invalid argument, even though conversion is successful."""
 
-class ResultsNotFound(commands.UserInputError):
+class ResultsNotFound(OutputtableException):
     """Exception raised when a search returns some form of "not found" """
 
 def private_message_only(error_msg="This command can only be used in private messages"):
     def predicate(ctx):
-        if isinstance(ctx.channel, discord.PrivateChannel):
+        if isinstance(ctx.channel, (discord.GroupChannel, discord.DMChannel)):
             return True
         raise PrivateMessagesOnly(error_msg)
     return commands.check(predicate)
