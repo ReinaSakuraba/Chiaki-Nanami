20d8cd6da459a5f53edf3dfd92c77b63b8a6b109
Ikusaba <Ikusaba-san@users.noreply.github.com>
2017-05-13 22:47:01 -0700

    added eval/exec; removed external reloading
    reloading is extremely dangerous because of the risk of zombie objects,
    since the globals for each module that have imported the old modules aren't updated.
    I'm better off just finding a better way to restart the bot.

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,9 +1,12 @@
 import argparse
 import asyncio
+import contextlib
 import discord
 import inspect
+import io
 import importlib
 import sys
+import textwrap
 import traceback
 
 from discord.ext import commands
@@ -12,6 +15,7 @@ from .utils import checks
 from .utils.converter import item_converter
 from .utils.context_managers import temp_attr
 from .utils.misc import code_msg
+from .utils.paginator import EmbedPages
 
 class Owner:
     """Owner-only commands"""
@@ -23,6 +27,7 @@ class Owner:
         # As __local_check isn't shown in the actual command
         # TODO: Work with __local_check
         self.bot = bot
+        self._last_result = None
 
     async def _load(self, ctx, ext):
         try:
@@ -34,12 +39,8 @@ class Owner:
         else:
             await ctx.send(code_msg(f'load {ext} successful'))
 
-    @commands.command(hidden=True)
-    async def debug(self, ctx, *, code: str):
-        """Evaluates code."""
-        code = code.strip('` ')
-
-        env = {
+    def _create_env(self, ctx):
+        return {
             'bot': self.bot,
             'ctx': ctx,
             'message': ctx.message,
@@ -50,6 +51,12 @@ class Owner:
             **globals()
         }
 
+    @commands.command(hidden=True)
+    async def debug(self, ctx, *, code: str):
+        """Evaluates code."""
+        code = code.strip('` ')
+
+        env = self._create_env(ctx)
         try:
             result = eval(code, env)
             if inspect.isawaitable(result):
@@ -58,6 +65,54 @@ class Owner:
             await ctx.send(code_msg(traceback.format_exc(), 'py'))
         else:
             await ctx.send(code_msg(result, 'py'))
+
+    @staticmethod
+    def cleanup_code(body):
+        # remove ```py\n```
+        if body.startswith('```') and body.endswith('```'):
+            return '\n'.join(body.split('\n')[1:-1])
+
+        # remove `foo`
+        return body.strip('` \n')
+
+    @staticmethod
+    def get_syntax_error(e):
+        if e.text is None:
+            return '```py\n{0.__class__.__name__}: {0}\n```'.format(e)
+        return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
+
+    @commands.command(hidden=True, name='eval', aliases=['exec'])
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates more code"""
+        env = {**self._create_env(ctx), '_': self._last_result}
+        body = self.cleanup_code(body)
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await ctx.send(self.get_syntax_error(e))
+
+        func = env['func']
+        with io.StringIO() as stdout:
+            try:
+                with contextlib.redirect_stdout(stdout):
+                    ret = await func()
+            except Exception as e:
+                value = stdout.getvalue()
+                await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+            else:
+                value = stdout.getvalue()
+                with contextlib.suppress(BaseException):
+                    await ctx.message.add_reaction('\u2705')
+
+                if ret is None:
+                    if value:
+                        await ctx.send(f'```py\n{value}\n```')
+                else:
+                    self._last_result = ret
+                    await ctx.send(f'```py\n{value}{ret}\n```')
+
 
     @commands.command(hidden=True)
     async def botav(self, ctx, *, avatar):
@@ -70,36 +125,6 @@ class Owner:
         """Reloads a bot-extension (one with a setup method)"""
         self.bot.unload_extension(cog)
         await self._load(ctx, cog)
-
-    @staticmethod
-    async def _attempt_external_import(ctx, func, module, *, message):
-        try:
-            getattr(importlib, func)(module)
-        except Exception as e:
-            await ctx.send(f"Failed to {message} module")
-            raise
-        else:
-            await ctx.send("\N{THUMBS UP SIGN}")
-
-    @commands.command(hidden=True)
-    async def reloadext(self, ctx, module: item_converter(sys.modules)):
-        """Attempts to reload a non-extension module (one that doesn't have a setup method)
-
-        Do note that just because you successfully reload the module,
-        extensions that have imported said module will not automatically have the new one.
-        Make sure you reload them as well.
-        """
-        await self._attempt_external_import(ctx, 'reload', module, message='reload')
-
-    @commands.command(hidden=True)
-    async def loadext(self, ctx, module):
-        """Attempts to load a non-extension module (one that doesn't have a setup method)
-
-        Do note that just because you successfully reload the module,
-        extensions that have imported said module will not automatically have the new one.
-        Make sure you reload them as well.
-        """
-        await self._attempt_external_import(ctx, 'import_module', module, message='import')
 
     @commands.command(hidden=True)
     async def load(self, ctx, cog: str):
@@ -125,6 +150,7 @@ class Owner:
 
     @commands.command(hidden=True)
     async def say(self, ctx, *, msg):
+        await ctx.message.delete()
         # make sure commands for other bots (or even from itself) can't be executed
         await ctx.send(f"\u200b{msg}")
 
