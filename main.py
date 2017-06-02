c9022a98e8d96626970e3d8386cd8b745d1b7b0d
Ikusaba <eeveeday@gmail.com>
2017-06-01 20:29:14 -0700

    only restart if it exits with a restart code

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import json
 import subprocess
 import sys
 
 from functools import partial
+from itertools import takewhile
 
-def run(log_result=False):
+def _load_config():
+    with open('config.json') as f:
+        return json.load(f)
+
+# Hope I can find a way to not repeat myself here
+_config = _load_config()
+RESTART_CODE = _config.get('restart_code', 69)
+
+def _runner():
     python = sys.executable
     cmd = (python, 'chiaki.py')
-    for _ in iter(partial(subprocess.call, cmd), 0):
-        print("Resetting Chiaki...")
+    while True:
+        yield subprocess.call(cmd)
+
+def run(log_result=False):
+    for i, code in enumerate(takewhile(lambda code: code == RESTART_CODE, _runner()), start=1):
+        print(f"Resetting Chiaki Attempt #{i}...")
 
 if __name__ == '__main__':
     run()
