cac1dd8a8825b720fdffd08b0f5c1ff15d6b55b2
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-06-21 23:12:20 -0700

    made the animal random; changed the end screen to show the top 3 racers
    Because of the former thing I had to use a class for once. So a bunch of
    code was slightly refactored and rewritten in the process.

--- cogs/games/race.py
+++ cogs/games/race.py
@@ -1,6 +1,8 @@
 import asyncio
 import contextlib
 import discord
+import heapq
+import operator
 import random
 import time
 
@@ -10,81 +12,131 @@ from discord.ext import commands
 from .manager import SessionManager
 from ..utils.misc import str_join
 
+
+TRACK_LENGTH = 40
+DEFAULT_TRACK = '|' + '-' * TRACK_LENGTH + '|'
+ANIMALS = [
+    '\N{TURTLE}',
+    '\N{SNAIL}',
+    '\N{ELEPHANT}',
+    '\N{RABBIT}',
+    '\N{PIG}'
+]
+
+
+class Racer:
+    def __init__(self):
+        self.animal = random.choice(ANIMALS)
+        self.distance = 0
+
+    def update(self):
+        self.distance += random.triangular(0, 10, 3)
+
+    def is_finished(self):
+        return self.distance >= TRACK_LENGTH + 1
+
+    @property
+    def progress(self):
+        buffer = DEFAULT_TRACK
+        position = round(self.distance)
+        return buffer[:position] + self.animal + buffer[position:]
+
+    @property
+    def position(self):
+        return self.distance / TRACK_LENGTH * 100
+
+
 class RacingSession:
-    TRACK_LENGTH = 20
-    DEFAULT_TRACK = '|' + '-' * TRACK_LENGTH + '|'
     MINIMUM_REQUIRED_MEMBERS = 2
     # fields can only go up to 25
     MAXIMUM_REQUIRED_MEMBERS = 25
 
     def __init__(self, ctx):
         self.ctx = ctx
-        self.positions = OrderedDict()
+        self.players = OrderedDict()
         self.running = False
-        self.current_embed = (discord.Embed(colour=self.ctx.bot.colour)
-                             .set_author(name='Race has started!')
-                             )
+        self._track = (discord.Embed(colour=self.ctx.bot.colour)
+                      .set_author(name='Race has started!')
+                      .set_footer(text='Current Leader: None')
+                      )
         self._is_full = asyncio.Event()
-        self.start = time.perf_counter()
 
     def add_member(self, m):
-        self.positions[m] = 1
-        if len(self.positions) >= self.MAXIMUM_REQUIRED_MEMBERS:
+        self.players[m] = Racer()
+        if len(self.players) >= self.MAXIMUM_REQUIRED_MEMBERS:
             self._is_full.set()
 
     def already_joined(self, m):
-        return m in self.positions
+        return m in self.players
 
     def has_enough_members(self):
-        return len(self.positions) >= self.MINIMUM_REQUIRED_MEMBERS
+        return len(self.players) >= self.MINIMUM_REQUIRED_MEMBERS
 
     def update_game(self):
-        for member in self.positions:
-            self.positions[member] = self.positions[member] + random.uniform(0, 3)
+        for player in self.players.values():
+            player.update()
 
     def _member_fields(self):
-        for member, position in self.positions.items():
-            buffer = self.DEFAULT_TRACK
-            position = round(position)
-            progression = buffer[:position] + '\N{TURTLE}' + buffer[position:]
-            yield {'name': str(member), 'value': progression}
+        return ((str(member), racer.progress) for member, racer in self.players.items())
 
     def update_current_embed(self):
-        for i, kwargs in enumerate(self._member_fields()):
-            self.current_embed.set_field_at(i, **kwargs, inline=False)
+        for i, (name, value) in enumerate(self._member_fields()):
+            self._track.set_field_at(i, name=name, value=value, inline=False)
+
+        leader = self.leader
+        position = min(self.players[leader].position, 100)
+        self._track.set_footer(text=f'Current Leader: {leader} ({position: .2f}m)')
 
     def winners(self):
-        return [member for member, position in self.positions.items()
-                if round(position) >= self.TRACK_LENGTH + 1]
+        return [m for m, r in self.players.items() if r.is_finished()]
 
     async def wait_until_full(self):
         await self._is_full.wait()
 
-    async def run(self):
-        for kwargs in self._member_fields():
-            self.current_embed.add_field(**kwargs, inline=False)
-        message = await self.ctx.send(embed=self.current_embed)
+    async def _loop(self):
+        for name, value in self._member_fields():
+            self._track.add_field(name=name, value=value, inline=False)
+        message = await self.ctx.send(embed=self._track)
         self.running = True
 
         while True:
             await asyncio.sleep(random.uniform(1, 3))
             self.update_game()
             self.update_current_embed()
-            await message.edit(embed=self.current_embed)
-            winners = self.winners()
-            if winners:
+            await message.edit(embed=self._track)
+            if self.is_completed():
                 break
 
-        seconds = time.perf_counter() - self.start
-        names = [str(m) for m in winners]
-        if len(names) > 1:
-            winners_fmt = ', '.join(names[:-1]) + ', and ' + names[-1]
-            extra_winner_text = 'are the winners!'
-        else:
-            winners_fmt = names[0]
-            extra_winner_text = 'is the winner!'
+    async def _display_winners(self):
+        names = ['Winner', 'Runner Up', 'Third Runner']
+        embed = discord.Embed(title='Results', colour=0x00FF00)
+        # Cannot use '\N' because the medal characters don't have a name
+        # I can only refer to them by their code points.
+        for title, (char, (member, racer)) in zip(names, enumerate(self.top_racers(), start=0x1f947)):
+            use_flag = "\N{CHEQUERED FLAG}" * racer.is_finished()
+            # We have to bold just the username in the event of a win.
+            # So we can't just f'{member}' here.
+            name = f'{title} {use_flag}'
+            value = f'{chr(char)} {racer.animal} {member}'
+            embed.add_field(name=name, value=value, inline=False)
 
-        await self.ctx.send(f'{winners_fmt} {extra_winner_text}')
+        await self.ctx.send(embed=embed)
+
+    async def run(self):
+        start = time.perf_counter()
+        await self._loop()
+        seconds = time.perf_counter() - start
+        await self._display_winners()
+
+    def top_racers(self, n=3):
+        return heapq.nlargest(n, self.players.items(), key=lambda i: i[1].distance)
+
+    def is_completed(self):
+        return any(r.is_finished() for r in self.players.values())
+
+    @property
+    def leader(self):
+        return max(self.players, key=lambda m: self.players[m].distance)
 
     async def stop(self, force=True):
         pass
@@ -105,19 +157,23 @@ class Racing:
                 return await ctx.send("You're already in the race!")
 
             session.add_member(ctx.author)
-            return await ctx.send('ok')
+            return await ctx.send(f"Okay, {ctx.author.mention}. Good luck!")
 
         with self.manager.temp_session(ctx.channel, RacingSession(ctx)) as inst:
             inst.add_member(ctx.author)
             await ctx.send(f'Race has started! Type {ctx.prefix}{ctx.invoked_with} to join!')
 
             with contextlib.suppress(asyncio.TimeoutError):
-                await asyncio.wait_for(inst.wait_until_full(), timeout=15)
+                await asyncio.wait_for(inst.wait_until_full(), timeout=30)
 
             if not inst.has_enough_members():
                 return await ctx.send("Can't start race. Not enough people :(")
             await asyncio.sleep(random.uniform(0.25, 0.75))
             await inst.run()
+
+    async def race_close(self):
+        """Stops registration of a race early."""
+        pass
 
 def setup(bot):
     bot.add_cog(Racing(bot))
