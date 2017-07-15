3ed2643e516f79632dc81b4291dd86565c6a77ce
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-07-15 12:09:09 -0700

    fixed Scheduler.remove_entry not removing the entry properly
    Turns out if the entry was the current entry, it would attempt to remove
    an entry that didn't exist.
    
    Also added Scheduler.stop, and Scheduler.close now removes all the entries

--- cogs/utils/timer.py
+++ cogs/utils/timer.py
@@ -5,6 +5,7 @@ import logging
 
 from collections import namedtuple
 from datetime import datetime
+from more_itertools import consume, iter_except
 
 from .misc import duration_units
 
@@ -36,6 +37,8 @@ class Scheduler:
         self.dispatch = dispatch
 
         self.pending = asyncio.PriorityQueue()
+        self._current = None
+        self._next_timestamp = None
         self._runner = self.loop.create_task(self._update())
 
     def __del__(self):
@@ -51,7 +54,6 @@ class Scheduler:
             while delta > 0:
                 await asyncio.sleep(min(self.MAX_SLEEP_TIME, delta))
                 delta -= self.MAX_SLEEP_TIME
-
             log.debug('entry %r is done, dispatching now.', timer)
             self.bot.dispatch(self.dispatch, timer)
 
@@ -63,26 +65,40 @@ class Scheduler:
 
     def add_entry(self, entry):
         """Adds a new timestamp entry for the queue."""
-        if hasattr(self, '_next_timestamp') and entry.when <= self._next_timestamp:
-            self._reset_task()
-            self.pending.put_nowait(self._current)
-
         self.pending.put_nowait(entry)
+
+        if self._next_timestamp and entry.when <= self._next_timestamp:
+            self.pending.put_nowait(self._current)
+            self._reset_task()
 
     def remove_entry(self, entry):
         """Removes a timestamp entry from the queue."""
         # Warning: Horribly bad practice, but there's no easy way to do this otherwise
-        self.pending._queue.remove(entry)
-        heapq.heapify(self.pending._queue)
-        # Don't put the current thing back in
-        if self._current != entry:
+
+        # The current entry was already popped from the queue, don't try to remove it again.
+        if entry != self._current:
+            self.pending._queue.remove(entry)
+            heapq.heapify(self.pending._queue)
             self.pending.put_nowait(self._current)
-        # We've modified the queue heavily here. So this task isn't really good anymore
+
+        # There are two things that could've happened here:
+        # 1. The entry was removed, and the internal list was reset
+        # 2. The entry was the current entry, and we did nothing
+        # Either way we have to reset the task, as the current one isn't good anymore.
         self._reset_task()
 
-    def close(self):
-        """Closes the running task."""
+    def stop(self):
+        """Stops the scheduler.
+
+        This doesn't clear all the entries, use close() for that.
+        """
         if not self._runner.done():
             with contextlib.suppress(BaseException):
                 self._runner.cancel()
+
+    def close(self):
+        """Closes the running task, and removes all the entries."""
+        self.stop()
+        consume(iter_except(self.pending.get_nowait, asyncio.QueueEmpty))
+        self._current = None
 
