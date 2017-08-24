c100b1a3d18e99ee695fd48ce80f088b321dafa4
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-08-23 18:50:16 -0700

    Remove ->contact and friends
    No one uses these commands, especially when there's a support server
    for the bot. These will probably be reimplemented with an actual
    feedback/issue channel.

--- cogs/halp.py
+++ cogs/halp.py
@@ -1,26 +1,17 @@
 import discord
-import json
-import operator
-import re
-import textwrap
 
-from collections import namedtuple
-from datetime import datetime
 from discord.ext import commands
-from functools import partial
 
-from .utils import errors
-from .utils.context_managers import temp_attr
 from .utils.converter import BotCogConverter, BotCommand
-from .utils.database import Database
 from .utils.formats import multi_replace
-from .utils.misc import nice_time, truncate
+from .utils.misc import truncate
 
 
 def default_help_command(func=lambda s: s, **kwargs):
     async def help_command(self, ctx, *, command: BotCommand=None):
         await default_help(ctx, command, func=func)
     return commands.command(help=func("Shows this message and stuff"), **kwargs)(help_command)
+
 
 async def default_help(ctx, command=None, func=lambda s:s):
     command = ctx.bot if command is None else command
@@ -34,81 +25,18 @@ async def default_help(ctx, command=None, func=lambda s:s):
         await destination.send(page)
 
 
-_message_attrs = 'id author content channel guild created_at'.split()
-DummyMessage = namedtuple('DummyMessage', _message_attrs)
-
-# Seriously, fuck JSONs. They can't do namedtuples
-class ProblemMessage:
-    __slots__ = tuple(_message_attrs)
-
-    def __init__(self, msg):
-        for attr in _message_attrs:
-            setattr(self, attr, getattr(msg, attr))
-
-    @property
-    def embed(self):
-        author = self.author
-        author_avatar = author.avatar_url_as(format=None)
-        id_fmt = "{0}\n({0.id})"
-        server_fmt = id_fmt.format(self.guild) if self.guild else "No server"
-
-        return (discord.Embed(timestamp=self.created_at)
-               .set_author(name=str(author), icon_url=author_avatar)
-               .set_thumbnail(url=author_avatar)
-               .add_field(name="Channel:", value=id_fmt.format(self.channel))
-               .add_field(name="Server:", value=server_fmt)
-               .add_field(name="Message:", value=self.content, inline=False)
-               .set_footer(text=f"Message ID: {self.id}")
-                )
-
-class ProblemEncoder(json.JSONEncoder):
-    def default(self, o):
-        if type(o) is ProblemMessage:
-            return {
-                '__problem__': True,
-                'id': o.id,
-                'author': o.author.id,
-                'content': o.content,
-                'channel': o.channel.id,
-                'server': getattr(o.guild, "id", None),
-                'created_at': str(o.created_at),
-                }
-        return super().default(o)
-
-def problem_hook(bot, dict_):
-    if '__problem__' in dict_:
-        kwargs = {
-            'id': dict_['id'],
-            'author': bot.get_user(dict_['author']),
-            'content': dict_['content'],
-            'channel': bot.get_channel(dict_['channel']),
-            'guild': bot.get_guild(dict_['server']),
-            'created_at': datetime.strptime(dict_['created_at'], '%Y-%m-%d %H:%M:%S.%f'),
-            }
-        return ProblemMessage(DummyMessage(**kwargs))
-    return dict_
-
 _bracket_repls = {
     '(': ')', ')': '(',
     '[': ']', ']': '[',
     '<': '>', '>': '<',
 }
 
+
 class Help:
     def __init__(self, bot):
         self.bot = bot
         self.bot.remove_command('help')
         self.bot.remove_command('h')
-        self.bot.loop.create_task(self.load_problems())
-
-    async def load_problems(self):
-        # because of the use of Client.get_* methods, we have to wait until the bot is logged in
-        # otherwise we'd just get Nones everywhere
-        await self.bot.wait_until_ready()
-        self.problems = Database('issues.json', encoder=ProblemEncoder, load_later=True,
-                                 object_hook=partial(problem_hook, self.bot))
-        self.blocked = self.problems.setdefault('blocked', [])
-        self.bot.add_database(self.problems)
 
     help = default_help_command(name='help', aliases=['h'])
     halp = default_help_command(str.upper, name='halp', aliases=['HALP'])
@@ -132,61 +60,6 @@ class Help:
                  )
         await ctx.send(embed=invite)
 
-    @commands.command()
-    @commands.cooldown(rate=1, per=30, type=commands.BucketType.user)
-    async def contact(self, ctx, *, problem: str):
-        """Contacts the bot owner
-
-        Try not to abuse this, as the owner can block you at will.
-        """
-        msg = ctx.message
-        author = msg.author
-        if author.id in self.blocked:
-            return await ctx.send("{ctx.author.mention}, you have been blocked from contacting the owner")
-
-        # TODO, make this work with namedtuples
-        with temp_attr(msg, 'content', msg.content.replace(f'{ctx.prefix}contact', '', 1)):
-            problem_message = ProblemMessage(msg)
-
-        self.problems[msg] = problem_message
-        await self.bot.owner.send(f"**{self.bot.owner.mention} New message from {author}!**",
-                                  embed=problem_message.embed)
-
-    @commands.command()
-    @commands.is_owner()
-    @errors.private_message_only()
-    async def contactblock(self, ctx, *, user: discord.User):
-        self.blocked.append(user.id)
-        await ctx.send(f"Blocked user {user} successfully!")
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    @errors.private_message_only()
-    async def answer(self, ctx, id_: int, *, response: str):
-        problem_message = self.problems.pop(id_, None)
-        if problem_message is None:
-            raise errors.ResultsNotFound(f"Message ID ***{id_}*** doesn't exist, I think")
-
-        avatar = self.bot.owner.avatar_url_as(format=None)
-        response_embed = (discord.Embed(colour=0x00FF00, description=response, timestamp=ctx.message.created_at)
-                         .set_author(name=str(self.bot.owner), icon_url=avatar)
-                         .set_thumbnail(url=self.bot.appinfo.icon_url)
-                         )
-
-        msg = f"{problem_message.author.mention}, you have a response for message ***{id_}***:"
-        await problem_message.author.send(msg, embed=response_embed)
-        await problem_message.channel.send(msg, embed=response_embed)
-        await ctx.send(f"Successfully responded to {id_}! Response:", embed=response_embed)
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    @errors.private_message_only()
-    async def review(self, ctx, id_: str):
-        problem_message = self.problems.get(id_)
-        if problem_message:
-            await ctx.send(f"**Saved Message from {problem_message.author}:**", embed=problem_message.embed)
-        raise errors.ResultsNotFound(f"ID {id_} doesn't exist, I think")
-
     @commands.command(aliases=['cogs', 'mdls'])
     async def modules(self, ctx):
         """Shows all the *visible* modules that I have loaded"""
@@ -207,6 +80,7 @@ class Help:
         commands_embeds = await self.bot.formatter.format_help_for(ctx, cog)
         for embed in commands_embeds:
             await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Help(bot))
