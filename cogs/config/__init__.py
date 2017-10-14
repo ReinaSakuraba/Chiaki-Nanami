15a2312afcafd52372d596c1ce08c53e5339882e
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-10-13 21:50:40 -0700

    Properly call the Permission cog's checks in Config

--- cogs/config/__init__.py
+++ cogs/config/__init__.py
@@ -5,7 +5,13 @@ from .welcome import WelcomeMessages
 
 class Config(Prefixes, Permissions, WelcomeMessages):
     """Commands related to any sort of configuration for Chiaki, i.e. me."""
-    pass
+
+    # TODO: Make a mixin-class for this
+    async def __global_check(self, ctx):
+        return await self._Permissions__global_check(ctx)
+
+    async def __global_check_once(self, ctx):
+        return await self._Permissions__global_check_once(ctx)
 
 def setup(bot):
     bot.add_cog(Config(bot))
